d09e9d191060834c52884ba61e03622983a8cb4c
Katorly <70022443+katorly@users.noreply.github.com>
2021-08-09 22:38:35 +0800

    Update Updater.java

--- Updater.java
+++ Updater.java
@@ -1,10 +1,7 @@
-import org.bukkit.Bukkit;
-
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
-import java.text.SimpleDateFormat;
 
 public class Updater {
     public void check() {
