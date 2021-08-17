300e5174cec104f7ce7a97a6d24b6deb26920200
katorly <1649643021@qq.com>
2021-08-17 21:10:42 +0800

    import imports

--- Updater.java
+++ Updater.java
@@ -1,12 +1,15 @@
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 
-public class Updater {
-    public void check() {
-        String currentversion = this.getDescription().getVersion();
-        getLogger().info("Checking update...");
+public class PluginUpdater {
+    public static void check(JavaPlugin plugin) {
+        String currentversion = plugin.getDescription().getVersion();
+        Bukkit.getLogger().info("Checking update...");
         try {
             URL url = new URL("https://raw.githubusercontent.com/username/repo-name/master/latest.txt");
             InputStream is = url.openStream();
